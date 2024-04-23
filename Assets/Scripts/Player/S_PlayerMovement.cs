4d96a18737449fc8ef7c367191aa91c23724c919
lukabartlo <l.bartolo@lahorde.school>
2024-04-23 12:24:46 +0200

    Push AnimationPersonage

--- Assets/Scripts/Player/S_PlayerMovement.cs
+++ Assets/Scripts/Player/S_PlayerMovement.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 1000f;
     private Vector2 mouvementInput;
 
     [SerializeField] private Vector2 boxSize;
     [SerializeField] private float castDistance;
     [SerializeField] private LayerMask obstacleLayer;
+    private Rigidbody2D rb2D;
+    private Animator animator;
+
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite spriteFront;
+    [SerializeField] private Sprite spriteSide;
+    [SerializeField] private Sprite spriteBack;
+    
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
+    }
 
     void FixedUpdate()
     {
@@ -23,7 +39,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void PerformMouvement()
     {
-        gameObject.transform.Translate(new Vector3(mouvementInput.x, mouvementInput.y, 0) * Time.deltaTime * speed);
+        rb2D.AddForce(new Vector3(mouvementInput.x, mouvementInput.y, 0) * Time.deltaTime * speed);
+        animator.SetFloat("Velocity", rb2D.velocity.magnitude);
     }
 
     public void Mouvement(InputAction.CallbackContext context)
@@ -31,6 +48,24 @@ public class PlayerMovement : MonoBehaviour
         if (context.performed)
         {
             mouvementInput = context.ReadValue<Vector2>();
+            if (mouvementInput.x == 1)
+            {
+                spriteRenderer.sprite = spriteSide;
+                spriteRenderer.flipX = false;
+            }
+            else if(mouvementInput.x == -1)
+            {
+                spriteRenderer.sprite = spriteSide;
+                spriteRenderer.flipX = true;
+            }
+            else if (mouvementInput.y == 1)
+            {
+                spriteRenderer.sprite = spriteBack;
+            }
+            else if (mouvementInput.y == -1)
+            {
+                spriteRenderer.sprite = spriteFront;
+            }
         }
     }
 
