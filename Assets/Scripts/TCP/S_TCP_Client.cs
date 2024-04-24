82178d1b617e7a3e7b101a60c7ba3b0b1beb0fb9
hdenise-lahorde <60253055+Auros1805@users.noreply.github.com>
2024-04-24 14:58:54 +0200

    mise en place de l'interpreter

--- Assets/Scripts/TCP/S_TCP_Client.cs
+++ Assets/Scripts/TCP/S_TCP_Client.cs
@@ -5,26 +5,54 @@ using System.Text;
 using System.Threading;
 using System;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
-public class TCP_Client : MonoBehaviour
+public class S_TCP_Client : MonoBehaviour
 {
-    private TcpClient client;
-    private NetworkStream stream;
-    private CancellationTokenSource cts;
-    public int discoveryPort = 8888;
-    private Thread serverlistenerThread;
-    private Thread discoveryThread;
-    private Thread connectionThread;
-    private string hostIP;
-    private Dictionary<string, Action> functionMap = new Dictionary<string, Action>();
+    private TcpClient _client;
+    private NetworkStream _stream;
+    private CancellationTokenSource _cts;
+    public int _discoveryPort = 8888;
+    private Thread _serverlistenerThread;
+    private Thread _discoveryThread;
+    private Thread _connectionThread;
+    private string _hostIP;
+    private Dictionary<string, Action> _functionMap = new Dictionary<string, Action>();
+    private string _loadSceneName = null;
 
+    public static S_TCP_Client _TCP_Instance { get; private set; }
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (_TCP_Instance == null)
+        {
+            _TCP_Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
 
     private void Start()
     {
+        _functionMap.Add("MegaMindWin", MegaMindWin);
+
+
         DontDestroyOnLoad(this.gameObject);
-        discoveryThread = new Thread(new ThreadStart(StartDiscovery));
-        discoveryThread.Start();
+        _discoveryThread = new Thread(new ThreadStart(StartDiscovery));
+        _discoveryThread.Start();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_loadSceneName != null && SceneManager.GetSceneByName(_loadSceneName) != null)
+        {
+            SceneManager.LoadScene(_loadSceneName);
+            _loadSceneName = null;
+        }
     }
 
     void StartDiscovery()
@@ -36,7 +64,7 @@ public class TCP_Client : MonoBehaviour
 
         // Envoyer une demande de découverte en diffusion
         byte[] discoverData = Encoding.UTF8.GetBytes("DISCOVER_MY_SERVICE");
-        udpClient.Send(discoverData, discoverData.Length, new IPEndPoint(IPAddress.Broadcast, discoveryPort));
+        udpClient.Send(discoverData, discoverData.Length, new IPEndPoint(IPAddress.Broadcast, _discoveryPort));
 
         // Attendre la réponse du serveur de découverte
         IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 0);
@@ -46,9 +74,9 @@ public class TCP_Client : MonoBehaviour
         // Analyser la réponse pour obtenir l'adresse IP de l'hôte
         if (response.StartsWith("HOST_IP:"))
         {
-            hostIP = response.Substring("HOST_IP:".Length);
-            connectionThread = new Thread(new ThreadStart(ConnectToServer));
-            connectionThread.Start();
+            _hostIP = response.Substring("HOST_IP:".Length);
+            _connectionThread = new Thread(new ThreadStart(ConnectToServer));
+            _connectionThread.Start();
         }
         else
         {
@@ -60,44 +88,45 @@ public class TCP_Client : MonoBehaviour
     private void ConnectToServer()
     {
         
-        Debug.Log("Service trouvé à l'adresse IP : " + hostIP);
+        Debug.Log("Service trouvé à l'adresse IP : " + _hostIP);
 
         // Connectez-vous à l'hôte en utilisant l'adresse IP trouvée
         // Implémentez votre code de connexion ici
 
         // Connexion au serveur TCP
-        client = new TcpClient();
-        client.Connect(hostIP, discoveryPort);
-        stream = client.GetStream();
+        _client = new TcpClient();
+        _client.Connect(_hostIP, _discoveryPort);
+        _stream = _client.GetStream();
         
         Debug.Log("Connecté au serveur !");
 
         // Envoi de données au serveur
         string message = "Bonjour serveur !";
         byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-        stream.Write(messageBytes, 0, messageBytes.Length);
+        _stream.Write(messageBytes, 0, messageBytes.Length);
 
         // Lecture de la réponse du serveur
         byte[] buffer = new byte[1024];
-        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        int bytesRead = _stream.Read(buffer, 0, buffer.Length);
         string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
         Debug.Log("Réponse du serveur : " + dataReceived);
 
-        serverlistenerThread = new Thread(new ThreadStart(Listener));
-        serverlistenerThread.Start();
+        _serverlistenerThread = new Thread(new ThreadStart(Listener));
+        _serverlistenerThread.Start();
+        LoadMegaMind();
     }
 
     private void Listener()
     {
         Debug.Log("Début de l'écoute.");
         // Tant que la connexion est ouverte, écouter les messages du serveur
-        while (client.Connected)
+        while (_client.Connected)
         {
             try
             {
                 // Lecture des données envoyées par le serveur
                 byte[] buffer = new byte[1024];
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                int bytesRead = _stream.Read(buffer, 0, buffer.Length);
 
                 // Vérifier si des données ont été lues
                 if (bytesRead > 0)
@@ -105,6 +134,8 @@ public class TCP_Client : MonoBehaviour
                     // Convertir les données en chaîne de caractères et afficher le message
                     string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
                     Debug.Log("Message perso reçu du serveur : " + dataReceived);
+                    if(dataReceived.StartsWith("FUNCTION_NAME:") || dataReceived.StartsWith("LOAD_SCENE:"))
+                        Interpreter(dataReceived);
                 }
             }
             catch (Exception ex)
@@ -119,22 +150,28 @@ public class TCP_Client : MonoBehaviour
         Debug.Log("Connexion au serveur fermée. Arrêt de l'écoute.");
     }
 
-    public void Interpreter(string commande)
+    private void Interpreter(string commande)
     {
         string[] parts = commande.Split(':');
-        if(parts[0] == "FUNCTION_NAME" && functionMap.ContainsKey(parts[1]))
+        Debug.Log(parts[0]);
+        Debug.Log(parts[1]);
+        if(parts[0] == "FUNCTION_NAME" && _functionMap.ContainsKey(parts[1]))
         {
-            functionMap[parts[1]].Invoke();
+            _functionMap[parts[1]].Invoke();
+        }
+        if (parts[0] == "LOAD_SCENE")
+        {
+            _loadSceneName = parts[1];
         }
     }
 
     public void SenderCallFunction(string functionName)
     {
-        if (stream != null && client != null && client.Connected)
+        if (_stream != null && _client != null && _client.Connected)
         {
             string message = "FUNCTION_NAME:";
             byte[] messageBytes = Encoding.UTF8.GetBytes(message+functionName);
-            stream.Write(messageBytes, 0, messageBytes.Length);
+            _stream.Write(messageBytes, 0, messageBytes.Length);
             Debug.Log("Message envoyé au client : " + message+functionName);
         }
         else
@@ -143,10 +180,35 @@ public class TCP_Client : MonoBehaviour
         }
     }
 
+    public void SenderLoadScene(string sceneName)
+    {
+        if (_stream != null && _client != null && _client.Connected)
+        {
+            string message = "LOAD_SCENE:";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message + sceneName);
+            _stream.Write(messageBytes, 0, messageBytes.Length);
+            Debug.Log("Message envoyé au client : " + message + sceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Impossible d'envoyer le message. La connexion client est fermée.");
+        }
+    }
+
+    public void LoadMegaMind()
+    {
+        SenderLoadScene("MegaMind");
+    }
+
+    private void MegaMindWin()
+    {
+        Debug.Log("MegaMind WIN");
+    }
+
     void OnDestroy()
     {
         // Fermeture de la connexion
-        stream.Close();
-        client.Close();
+        _stream.Close();
+        _client.Close();
     }
 }
