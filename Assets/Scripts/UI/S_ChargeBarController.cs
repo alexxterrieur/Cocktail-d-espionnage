ea8205be88e24b60c3837d43f42a4ae3b1b1fe82
vmalvoisin <v.malvoisin@lahorde.school>
2024-04-29 10:41:20 +0200

    Update S_ChargeBarController.cs

--- Assets/Scripts/UI/S_ChargeBarController.cs
+++ Assets/Scripts/UI/S_ChargeBarController.cs
@@ -9,11 +9,17 @@ public class S_ChargeBarController : MonoBehaviour
     void Start()
     {
         _slider = GetComponent<Slider>();
+        _slider.maxValue = 200;
     }
 
     // Update is called once per frame
     void Update()
     {
         _slider.value = S_TCP_Client._TCP_Instance.JoltScore;
+       
+        if (_slider.value >= 200)
+        {
+            Debug.Log("BITE!");
+        }
     }
 }
