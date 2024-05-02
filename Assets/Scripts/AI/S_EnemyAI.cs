12b3d50fdd40c09444bd83fe93e542285eecc161
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2024-05-02 14:58:13 +0200

    ia visuels

--- Assets/Scripts/AI/S_EnemyAI.cs
+++ Assets/Scripts/AI/S_EnemyAI.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class S_EnemyAI : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class S_EnemyAI : MonoBehaviour
     public float sightRange;
     public Transform[] waypoints;
     public Node start;
+
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite spriteFront;
+    [SerializeField] private Sprite spriteSide;
+    [SerializeField] private Sprite spriteBack;
 
     void Start()
     {
@@ -25,17 +31,39 @@ public class S_EnemyAI : MonoBehaviour
         S_CloseEnough closeEnough = new S_CloseEnough(agent, player, closeRange);
         S_DoSomething doSomething = new S_DoSomething();
 
-        Sequence sequence1 = new Sequence( new List<Node> { closeEnough, doSomething });
-        Selector selector1 = new Selector( new List<Node> { sequence1, chasePlayer });
+        Sequence sequence1 = new Sequence(new List<Node> { closeEnough, doSomething });
+        Selector selector1 = new Selector(new List<Node> { sequence1, chasePlayer });
         Sequence sequence2 = new Sequence(new List<Node> { seePlayer, selector1 });
         Selector selector2 = new Selector(new List<Node> { sequence2, patrol });
 
         start = selector2;
 
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
     {
         start.Evaluate();
+
+        
+        Vector3 agentVelocity = agent.velocity.normalized;
+        if (agentVelocity.y > 0)
+        {
+            spriteRenderer.sprite = spriteBack;
+        }
+        else if (agentVelocity.y < 0)
+        {
+            spriteRenderer.sprite = spriteFront;
+        }
+        else if (agentVelocity.x > 0)
+        {
+            spriteRenderer.sprite = spriteSide;
+            spriteRenderer.flipX = false;
+        }
+        else if (agentVelocity.x < 0)
+        {
+            spriteRenderer.sprite = spriteSide;
+            spriteRenderer.flipX = true;
+        }
     }
 }
